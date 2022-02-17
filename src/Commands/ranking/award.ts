01ef3cb78cf8f4bc08646f3ac1afe04f822a08ba
salahmak <salah.mak@protonmail.com>
2022-02-17 22:34:52 +0100

    code refactor to fix promise resolving late related bugs

--- src/Commands/ranking/award.ts
+++ src/Commands/ranking/award.ts
@@ -1,82 +1,93 @@
-import { Client, Message } from "discord.js";
-import { Command, Register, User } from "../../Interfaces";
+import { Message } from "discord.js";
+import { Command } from "../../Interfaces";
 import register from "../../utils/register";
 import UserModel from "../../models/user";
 
 import { ExtendedClient } from "../../Client/index";
-
 export const command: Command = {
     name: "award",
     description:
         "used by admins to give points to users when they solve a challenge",
     usage: `\`award @users <amount>\``,
     run: async (client: ExtendedClient, msg: Message, args: string[]) => {
-        try {
-            if (!msg.member!.permissions!.has("ADMINISTRATOR")) {
-                msg.channel.send(
-                    `<@${msg.author.id}> You don't have access to this command, if you have solved a challenge, please ask admins to award you, thanks.`
-                );
-                return;
-            }
+        if (!msg.member!.permissions!.has("ADMINISTRATOR")) {
+            msg.channel.send(
+                `<@${msg.author.id}> You don't have access to this command, if you have solved a challenge, please ask admins to award you, thanks.`
+            );
+            return;
+        }
 
-            let ids: string[] = [];
-            const server = msg.guildId!.toString();
-            const points = +args[args.length - 1];
+        //let ids: string[] = [];
+        const server = msg.guildId!.toString();
+        const points = +args[args.length - 1];
 
-            if (!Boolean(points)) {
-                msg.channel.send("please enter a correct number");
-                return;
-            }
+        if (!Boolean(points)) {
+            msg.channel.send("please enter a correct number");
+            return;
+        }
 
-            //handling case when the given points are negative
-            if (points <= 0) {
-                msg.channel.send(
-                    `Please enter a positive integer (if you want to subtract points, use \`${client.config.prefix} remove\` instead`
-                );
-                return;
-            }
+        //handling case when the given points are negative
+        if (points <= 0) {
+            msg.channel.send(
+                `Please enter a positive integer (if you want to subtract points, use \`${client.config.prefix} remove\` instead`
+            );
+            return;
+        }
 
-            if (msg.mentions.users.size > 0) {
-                msg.mentions.users.forEach(async (user) => {
-                    //adding the current id to the ids array
-                    ids.push(`<@!${user.id}>`);
-
-                    //checking if the user exists already
-                    const userExists = await UserModel.findOne({
-                        id: user.id,
-                        server,
-                    });
-
-                    //if the user exists, we update their points
-                    if (userExists) {
-                        await userExists.updateOne({
-                            $inc: { score: points, solved: 1 },
+        if (msg.mentions.users.size > 0) {
+            let ids = await Promise.all(
+                msg.mentions.users.map(async (user) => {
+                    try {
+                        //checking if the user exists already
+                        const userExists = await UserModel.findOne({
+                            id: user.id,
+                            server,
                         });
-                    } else {
-                        //if the user doesn't exist, we create it and give it the points
-                        const newUser = await register(user.id, server);
-                        if (newUser.success) {
-                            await UserModel.findOneAndUpdate(newUser.user, {
+
+                        //if the user exists, we update their points
+                        if (userExists) {
+                            await userExists!.updateOne({
                                 $inc: { score: points, solved: 1 },
                             });
+                        } else {
+                            //if the user doesn't exist, we create it and give it the points
+                            const newUser = await register(user.id, server);
+                            if (newUser.success) {
+                                await UserModel.findOneAndUpdate(newUser.user, {
+                                    $inc: { score: points, solved: 1 },
+                                });
+                            }
                         }
+                        return `<@!${user.id}>`;
+                    } catch (e) {
+                        msg.channel.send(`error has occured in award.ts`);
+                        console.log("error has occured in award.ts");
                     }
-                });
-            } else {
-                msg.channel.send(
-                    `error: use as shown:\n\`${client.config.prefix} award @user <amount>\``
-                );
+                })
+            );
 
-                return;
+            if (ids!.includes(undefined)) {
+                msg.channel.send(
+                    "WARNING: perhaps someone wasn't awarded correctly, check again"
+                );
             }
 
+            //removing falsy values from the ids array
+            ids = ids.filter(Boolean);
+
+            if (ids.length !== 0) {
+                msg.channel.send(
+                    `${ids!.join(" and ")} ${
+                        ids!.length == 1 ? "has" : "have"
+                    } been awarded ${points} point`
+                );
+            }
+        } else {
             msg.channel.send(
-                `${ids!.join(" and ")} ${
-                    ids!.length == 1 ? "has" : "have"
-                } been awarded ${points} point`
+                `error: use as shown:\n\`${client.config.prefix} award @user <amount>\``
             );
-        } catch (e) {
-            console.log("error has occured in award.ts");
+
+            return;
         }
     },
     aliases: ["reward"],
--- src/Commands/ranking/remove.ts
+++ src/Commands/ranking/remove.ts
@@ -1,5 +1,5 @@
-import { Client, Message } from "discord.js";
-import { Command, Register, User } from "../../Interfaces";
+import { Message } from "discord.js";
+import { Command } from "../../Interfaces";
 import register from "../../utils/register";
 import UserModel from "../../models/user";
 
@@ -7,72 +7,91 @@ import { ExtendedClient } from "../../Client/index";
 
 export const command: Command = {
     name: "remove",
-    description: "used by admins to take away points from users",
+    description: "used by admins to take away points from users.",
     usage: `\`remove @users <amount>\``,
     run: async (client: ExtendedClient, msg: Message, args: string[]) => {
-        try {
-            if (!msg.member!.permissions!.has("ADMINISTRATOR")) {
-                msg.channel.send(
-                    `<@${msg.author.id}> You don't have access to this command, if you have solved a challenge, please ask admins to award you, thanks.`
-                );
-                return;
-            }
-
-            let ids: string[] = [];
-            const server = msg.guildId!.toString();
-            const points = +args[args.length - 1];
-
-            if (!Boolean(points)) {
-                msg.channel.send("please enter a correct number");
-                return;
-            }
-
-            //handling case when the given points are negative
-            if (points <= 0) {
-                msg.channel.send(
-                    `Please enter a positive integer (if you want to award points, use \`${client.config.prefix} award\` instead`
-                );
-                return;
-            }
-
-            if (msg.mentions.users.size > 0) {
-                msg.mentions.users.forEach(async (user) => {
-                    //adding the current id to the ids array
-                    ids.push(`<@!${user.id}>`);
-
-                    //checking if the user exists already
-                    const userExists = await UserModel.findOne({
-                        id: user.id,
-                        server,
-                    });
-
-                    //if the user exists, we update their points
-                    if (userExists) {
-                        if(userExists.score < points) {
-                            msg.channel.send(`user <@!${user.id}> has less points than ${points}`);
-                        }else{
-                            await userExists.updateOne({
-                                $inc: { score: -points, solved: -1 },
-                            });
-                        }
-                    } else {
-                        //if the user doesn't exist, we create it and give it the points
-                        msg.channel.send(`<@!${user.id}> isn't registered`);
-                    }
-                });
-            } else {
-                msg.channel.send(
-                    `error: use as shown:\n\`${client.config.prefix} remove @user <amount>\``
-                );
-
-                return;
-            }
-
+        if (!msg.member!.permissions!.has("ADMINISTRATOR")) {
             msg.channel.send(
-                `${points} point has been taken away from ${ids!.join(" and ")}`
+                `<@${msg.author.id}> You don't have access to this command.`
             );
-        } catch (e) {
-            console.log("error has occured in remove.ts");
+            return;
+        }
+
+        //let ids: string[] = [];
+        const server = msg.guildId!.toString();
+        const points = +args[args.length - 1];
+
+        if (!Boolean(points)) {
+            msg.channel.send("please enter a correct number");
+            return;
+        }
+
+        //handling case when the given points are negative
+        if (points <= 0) {
+            msg.channel.send(
+                `Please enter a positive integer (if you want to award points, use \`${client.config.prefix} award\` instead`
+            );
+            return;
+        }
+
+        if (msg.mentions.users.size > 0) {
+            let ids = await Promise.all(
+                msg.mentions.users.map(async (user) => {
+                    try {
+                        //checking if the user exists already
+                        const userExists = await UserModel.findOne({
+                            id: user.id,
+                            server,
+                        });
+
+                        //if the user exists, we update their points
+                        if (userExists) {
+                            if (userExists.score < points) {
+                                msg.channel.send(
+                                    `user <@!${user.id}> has less points than ${points}`
+                                );
+                            } else {
+                                await userExists!.updateOne({
+                                    $inc: { score: -points, solved: -1 },
+                                });
+                            }
+                        } else {
+                            //if the user doesn't exist, we exit
+                            msg.channel.send(
+                                `user <@!${user.id}> doesn't exist`
+                            );
+                            return null;
+                        }
+                        return `<@!${user.id}>`;
+                    } catch (e) {
+                        msg.channel.send(`error has occured in remove.ts`);
+                        console.log("error has occured in remove.ts");
+                    }
+                })
+            );
+
+            if (ids!.includes(undefined)) {
+                msg.channel.send(
+                    "WARNING: perhaps someone's points wheren't removed correctly, check again"
+                );
+            }
+
+            //removing falsy values from the ids array
+            ids = ids.filter(Boolean)
+
+            if (ids.length !== 0) {
+                msg.channel.send(
+                    `${points} point has been taken away from ${ids!.join(
+                        " and "
+                    )}`
+                );
+            }
+        } else {
+            msg.channel.send(
+                `error: use as shown:\n\`${client.config.prefix} remove @user <amount>\``
+            );
+
+            return;
         }
     },
     aliases: ["rm"],
