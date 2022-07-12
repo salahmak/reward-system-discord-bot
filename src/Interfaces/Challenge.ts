afd8deb3943e8ed72f0808d4d8ba6800772852d8
salahmak <salah.mak@protonmail.com>
2022-07-12 18:39:39 +0100

    added name to the challenge's schema and interface

--- src/Interfaces/Challenge.ts
+++ src/Interfaces/Challenge.ts
@@ -1,4 +1,5 @@
 export interface Challenge {
+    name: string;           //name of the challenge (has to be unique)
     server: string;        //the id of the server that the challenge belongs to
     award: number;           //the points that the challenge gives
     solvedCount: number;     //number of times the challenge has been solved
--- src/models/challenge.ts
+++ src/models/challenge.ts
@@ -2,6 +2,7 @@ import { Challenge } from "../Interfaces/index";
 import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema<Challenge>({
+  name: String,
   server: String,
   award: Number,
   solvedCount: Number,
