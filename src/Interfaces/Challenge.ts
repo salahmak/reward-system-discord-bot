24f06e22c2744349d5665f808579e80fb1614195
salahmak <salah.mak@protonmail.com>
2022-07-12 18:28:49 +0100

    changed serverId to server in the challenge's interface and schema

--- src/Interfaces/Challenge.ts
+++ src/Interfaces/Challenge.ts
@@ -1,6 +1,5 @@
 export interface Challenge {
-    id: string;              //unique id of the challenge
-    serverId: string;        //the id of the server that the challenge belongs to
+    server: string;        //the id of the server that the challenge belongs to
     award: number;           //the points that the challenge gives
     solvedCount: number;     //number of times the challenge has been solved
 }
--- src/models/challenge.ts
+++ src/models/challenge.ts
@@ -2,8 +2,7 @@ import { Challenge } from "../Interfaces/index";
 import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema<Challenge>({
-  id: String,
-  serverId: String,
+  server: String,
   award: Number,
   solvedCount: Number,
 });
